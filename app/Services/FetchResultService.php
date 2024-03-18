5c86fe5fb37cfb6e73aec6ff37ca7e3a33a3afd5
kobe <youcode@skiff.com>
2024-03-18 18:50:17 +0000

    Fetching the results of the  Sphere Engine API response
    Signed-off-by: kobe <youcode@skiff.com>

---
+++ app/Services/FetchResultService.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+
+class FetchResultService
+{
+    protected $client;
+    protected $accessToken;
+    protected $endpoint;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->accessToken = env('SPHERE_ENGINE_ACCESS_TOKEN');
+        $this->endpoint = env('SPHERE_ENGINE_ENDPOINT');
+    }
+
+    public function fetchResult($submissionId)
+    {
+        $response = $this->client->get("{$this->endpoint}/submissions/{$submissionId}", [
+            'query' => ['access_token' => $this->accessToken],
+        ]);
+
+        $statusCode = $response->getStatusCode();
+        $content = json_decode($response->getBody()->getContents(), true);
+        Log::info('FetchResultService:fetchResult', ['statusCode' => $statusCode, 'content' => $content]);
+        if ($statusCode == 200 && isset($content['result']['streams']['output']['uri'])) {
+            $outputUri = $content['result']['streams']['output']['uri'];
+            $outputResponse = $this->client->get($outputUri);
+            Log::info('FetchResultService:fetchResult', ['outputResponse' => $outputResponse]);
+            if ($outputResponse->getStatusCode() == 200) {
+                $actualOutput = (string) $outputResponse->getBody();
+                $content['result']['actualOutput'] = $actualOutput;
+                Log::info('FetchResultService:fetchResult', ['actualOutput' => $actualOutput]);
+            }
+        }
+
+        if ($statusCode == 200) {
+            return [
+                'success' => true,
+                'data' => $content,
+            ];
+        } else {
+            return [
+                'success' => false,
+                'message' => 'Failed to fetch the result.',
+            ];
+        }
+    }
+}
