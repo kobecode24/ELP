c240d3ffd531eac84ce186b72fe732486d9532cc
kobe <youcode@skiff.com>
2024-03-31 11:29:58 +0000

    added the ChapterObserver ExerciseObserver LessonObserver to the boot method in the AppServiceProvider
    Signed-off-by: kobe <youcode@skiff.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Chapter;
+use App\Models\Exercise;
+use App\Models\Lesson;
+use App\Observers\ChapterObserver;
+use App\Observers\ExerciseObserver;
+use App\Observers\LessonObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Lesson::observe(LessonObserver::class);
+        Exercise::observe(ExerciseObserver::class);
+        Chapter::observe(ChapterObserver::class);
     }
 }
