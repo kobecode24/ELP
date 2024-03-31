a99c5dcc29fb516902c25e069a9b9cf6aab94b1b
kobe <youcode@skiff.com>
2024-03-31 10:06:17 +0000

    auth footer components
    Signed-off-by: kobe <youcode@skiff.com>

---
+++ resources/views/components/auth_footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="mt-16">
+    <div class="flex items-center md:justify-between justify-center">
+      <div class="w-full grid items-center justify-center px-5 mx-auto text-center">
+        <div class="grid grid-cols-2 gap-4 md:grid-cols-4 py-8">
+          <div class="text-[#282A35] text-sm dark:text-white font-normal py-1">
+            <h3 class="text-center">© 2024</h3>
+          </div>
+          <div class="text-[#282A35] text-sm dark:text-white font-normal py-1">
+            <h3 class="text-center">
+              <span class="underline">  ELP</span>. All rights reserved.
+            </h3>
+          </div>
+          <div class="underline text-[#282A35] text-sm dark:text-white font-normal py-1">
+            <h3 class="text-center">Cookie policy.</h3>
+          </div>
+          <div class="text-[#282A35] text-sm dark:text-white font-normal py-1">
+            <h3 class="text-center">
+              <span class="underline">Privacy and Terms</span>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
