c41fcd0394d5cc6551b1c17c4274309bf36a36cf
kobe <youcode@skiff.com>
2024-03-31 10:20:06 +0000

    app layout
    Signed-off-by: kobe <youcode@skiff.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,59 +1,72 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ "ElP" }} @yield('title')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.tiny.cloud/1/zrpcg1x4ynlays7o2fexsq6ecvj3spkuebyxobx20yepris3/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <script src="https://unpkg.com/cloudinary-video-player/dist/cld-video-player.min.js"></script>
-    <link href="https://unpkg.com/cloudinary-video-player/dist/cld-video-player.min.css" rel="stylesheet">
-</head>
-<body class="bg-gray-100 font-sans leading-normal tracking-normal">
-<nav class="bg-gray-800">
-    <div class="container mx-auto px-6 py-3">
-        <div class="flex items-center justify-between">
-            <div class="flex items-center">
-                <a href="{{--{{ route('user.projects.list') }}--}}" class="text-white text-lg font-semibold">ELP</a>
-                <ul class="flex items-center space-x-4 ml-10">
-                    <li><a href="{{--{{ route('home') }}--}}" class="text-gray-300 hover:text-white">Home</a></li>
-                    <li><a href="{{--{{ route('about') }}--}}" class="text-gray-300 hover:text-white">About</a></li>
-                    <li><a href="{{--{{ route('contact') }}--}}" class="text-gray-300 hover:text-white">Contact</a></li>
-                </ul>
-            </div>
-            @guest
-                <div>
-                    <a href="{{ route('login') }}" class="text-gray-300 hover:text-white">Login</a>
-                    <a href="{{ route('register') }}" class="ml-4 text-gray-300 hover:text-white">Register</a>
-                </div>
-            @else
-                <div>
-                    <span class="text-gray-300">{{ Auth::user()->name }}</span>
-                    <form action="{{ route('logout') }}" method="POST" class="inline">
-                        @csrf
-                        <button type="submit" class="ml-4 text-gray-300 hover:text-white">Logout</button>
-                    </form>
-                </div>
-            @endguest
-        </div>
-    </div>
-</nav>
+<html lang="en">
+    <header>
+        @include('components.head')
+    </header>
+  <body>
+    @include('components.home_navbar')
 
-<header class="bg-white shadow">
-    <div class="container mx-auto px-6 py-6">
-        <h1 class="font-semibold text-xl text-gray-800">@yield('header')</h1>
-    </div>
-</header>
-
-<main class="container mx-auto px-6 py-8">
+<main>
     @yield('content')
 </main>
 
-<footer class="bg-gray-800 text-white text-center p-4">
-    © {{ date('Y') }} {{'ELP' }}. All rights reserved.
+@include('components.footer')
 
-</footer>
 <script src="{{ asset('js/toggleDropdown.js') }}"></script>
-</body>
+<script
+type="text/javascript"
+src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js"
+></script>
+    <style>
+        .sticky-navbar {
+            position: sticky;
+            top: 0;
+            z-index: 50;
+        }
+    </style>
+<script>
+
+const data = {
+    datasets: [
+        {
+            data: [775, 948, 594],
+            backgroundColor: [
+                "rgba(217, 238, 225, 1)",
+
+                "rgba(34, 197, 94, 1)",
+                "rgba(255, 244, 163, 1)",
+            ],
+            borderColor: "white",
+            borderWidth: 4,
+            cutout: "87%",
+            circumference: 180,
+            rotation: 270,
+            borderRadius: 5,
+        },
+    ],
+};
+
+// config
+const config = {
+    type: "doughnut",
+    data,
+    options: {},
+};
+
+// render init block
+const myChart = new Chart(document.getElementById("myChart"), config);
+
+// Instantly assign Chart.js version
+const chartVersion = document.getElementById("chartVersion");
+chartVersion.innerText = Chart.version;
+
+</script>
+
+    <script>
+        tinymce.init({
+            selector: '#requirements',
+        });
+    </script>
+
+ </body>
 </html>
