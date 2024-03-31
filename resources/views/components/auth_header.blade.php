92a9edc82be128fa2fecbd0070d3afcd635f1ede
kobe <youcode@skiff.com>
2024-03-31 10:05:43 +0000

    auth header components
    Signed-off-by: kobe <youcode@skiff.com>

---
+++ resources/views/components/auth_header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="flex items-center px-4 md:px-10 py-5">
+      <div class="w-full h-full">
+          <a href="{{route('home')}}">
+        <img class="w-16 transform transition duration-300 ease-in-out hover:scale-110" src="https://res.cloudinary.com/hkjp5o9bu/image/upload/v1710910639/default_images/hlayp34ieplhtduyhlo9.png" alt="" />
+          </a>
+      </div>
+      <div>
+          <a href="{{ route('home') }}" class="group">
+              <svg
+                  class="ml-auto fill-current text-gray-700 group-hover:text-purple-500 w-6 h-6 cursor-pointer"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 18 18">
+                  <path
+                      d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/>
+                </svg>
+          </a>
+      </div>
+    </div>
+  </header>
