d0dc8313db823a2719f62de7c235f3c4c00815b4
kobe <youcode@skiff.com>
2024-03-31 10:15:16 +0000

    website profile navbar component
    Signed-off-by: kobe <youcode@skiff.com>

---
+++ resources/views/components/profile_navbar.blade.php
@@ -0,0 +1,27 @@
+<header class="px-7 flex w-full items-center justify-between border-b-2 border-gray-200 bg-white p-2">
+    <!-- logo -->
+
+    <div class="flex items-center space-x-2">
+        <a href="{{ route('home') }}" class="transform hover:scale-110">
+            <img class="w-10 md:w-12" src="https://res.cloudinary.com/hkjp5o9bu/image/upload/v1710910639/default_images/hlayp34ieplhtduyhlo9.png" alt="" />
+        </a>
+    </div>
+
+    <!-- button profile -->
+    <div class="flex justify-center items-center gap-5">
+        <div class="">
+            <h3 class="font-normal text-base">Student</h3>
+        </div>
+        <div class="relative">
+            <!-- User Avatar -->
+            <div class="w-9 h-9 flex items-center justify-center bg-[#2D2F31] rounded-full">
+                <img class="w-8 h-8 rounded-full" src="{{$user->profile_image_url}}" alt="User Avatar" />
+            </div>
+
+            <!-- Online Status Dot -->
+            <div class="absolute right-0 -top-1 h-4 w-4 rounded-full bg-[#A435F0]" title="User is online">
+            </div>
+        </div>
+    </div>
+</header>
+
