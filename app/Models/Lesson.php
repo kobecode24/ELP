63fe3fd56777bdebfbf98197b2c2aff0af5c666d
kobe <youcode@skiff.com>
2024-04-09 19:44:59 +0000

    added getImageAttribute and getFormattedVideoDurationAttribute methods for the Lesson Model
    Signed-off-by: kobe <youcode@skiff.com>

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -14,10 +14,30 @@ class Lesson extends Model
         'video_url',
         'chapter_id',
         'video_public_id',
+        'video_duration',
+        'is_video_processing',
     ];
 
     public function chapter()
     {
         return $this->belongsTo(Chapter::class);
+    }
+
+    public function getImageAttribute() {
+        return 'images/play.svg';
+    }
+
+    public function getFormattedVideoDurationAttribute()
+    {
+        $seconds = $this->video_duration ?? 0;
+        $hours = floor($seconds / 3600);
+        $minutes = floor(($seconds % 3600) / 60);
+        $seconds = $seconds % 60;
+
+        if ($hours > 0) {
+            return sprintf('%1$d:%2$02d:%3$02d', $hours, $minutes, $seconds);
+        } else {
+            return sprintf('%1$d:%2$02d', $minutes, $seconds);
+        }
     }
 }
