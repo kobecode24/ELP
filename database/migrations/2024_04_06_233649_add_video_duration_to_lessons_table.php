90df7525f5bc0e6bcbfa01e2eafb377431d99ccf
kobe <youcode@skiff.com>
2024-04-09 19:03:13 +0000

    added the video duration column to the Lessons table
    Signed-off-by: kobe <youcode@skiff.com>

---
+++ database/migrations/2024_04_06_233649_add_video_duration_to_lessons_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->integer('video_duration')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            //
+        });
+    }
+};
