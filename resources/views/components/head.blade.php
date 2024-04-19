1b4e15627f54b9396d0511517f888824412e2a1a
kobe <youcode@skiff.com>
2024-04-19 14:08:00 +0100

    added izi Toast cdn for alters view
    Signed-off-by: kobe <youcode@skiff.com>

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -7,7 +7,11 @@
     <script src="https://unpkg.com/cloudinary-video-player/dist/cld-video-player.min.js"></script>
     <link href="https://unpkg.com/cloudinary-video-player/dist/cld-video-player.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/izitoast/dist/css/iziToast.min.css">
+
     <script src="//unpkg.com/alpinejs" defer></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
     <link
     href="https://cdn.jsdelivr.net/npm/daisyui@4.7.2/dist/full.min.css"
