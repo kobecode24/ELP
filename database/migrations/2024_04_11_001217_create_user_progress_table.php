18ee41724c8334987667caf062e07559ae6f51bf
kobe <youcode@skiff.com>
2024-04-19 10:48:10 +0100

    added User Progress migration
    Signed-off-by: kobe <youcode@skiff.com>

---
+++ database/migrations/2024_04_11_001217_create_user_progress_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_progress', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->foreignId('lesson_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('exercise_id')->nullable()->constrained()->onDelete('cascade');
+            $table->timestamp('completed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_progress');
+    }
+};
