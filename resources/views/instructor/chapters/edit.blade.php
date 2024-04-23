b90df7bcf6e5666e7815253703425013650be84a
kobe <youcode@skiff.com>
2024-04-23 12:37:34 +0100

    added a container and updated the update btn
    Signed-off-by: kobe <youcode@skiff.com>

--- resources/views/instructor/chapters/edit.blade.php
+++ resources/views/instructor/chapters/edit.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <div class="container mx-auto p-4">
+    <div class="container mx-auto p-4 pt-32">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <h1 class="text-xl font-bold mb-4">Edit Chapter</h1>
         <form action="{{ route('instructor.chapters.update', $chapter->id) }}" method="POST">
             @csrf
@@ -26,7 +26,7 @@
                 <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description:</label>
                 <textarea name="description" id="description" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $chapter->description }}</textarea>
             </div>
-            <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Update Chapter</button>
+            <button type="submit" class="btn btn-primary">Update Chapter</button>
         </form>
     </div>
 @endsection
