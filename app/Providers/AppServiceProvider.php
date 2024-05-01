48e80debff9adfa790f65a3062443fce08317b8d
kobe <youcode@skiff.com>
2024-05-01 04:29:16 +0100

    AppServiceProvider viewPulse
    Signed-off-by: kobe <youcode@skiff.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\Lesson;
 use App\Observers\ChapterObserver;
 use App\Observers\ExerciseObserver;
 use App\Observers\LessonObserver;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
         Lesson::observe(LessonObserver::class);
         Exercise::observe(ExerciseObserver::class);
         Chapter::observe(ChapterObserver::class);
+
+        Gate::define('viewPulse', function ($user) {
+            return $user->isAdmin();
+        });
     }
 }
